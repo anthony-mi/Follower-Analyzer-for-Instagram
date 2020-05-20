d297ae6b0b8be10488e83d78f2608eaa9113ab96
Michael <m.kinder853@gmail.com>
2020-05-20 09:13:27 +0300

    Add project files.

---
+++ Follower Analyzer for Instagram/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Follower_Analyzer_for_Instagram.MvcApplication" Language="C#" %>
---
+++ Follower Analyzer for Instagram/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Follower_Analyzer_for_Instagram.Startup))]
+namespace Follower_Analyzer_for_Instagram
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
