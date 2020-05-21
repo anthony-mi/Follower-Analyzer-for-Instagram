b8dda028ca258f56279fafd3c1b51c9c32c8983f
Michael <m.kinder853@gmail.com>
2020-05-21 09:15:43 +0300

    1) Из AppStart удалены IdentityConfig.cs, Startap.Auth.cs 2) Из Models удален класс ApplicationUser.cs 3) В Models добавлены новые классы: User.cs, FollowerAnalizerContext 4) Соответственно изменена строка подключения в WebConfig

---
+++ Follower Analyzer for Instagram/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Follower_Analyzer_for_Instagram.Models
+{
+    public class User
+    {
+        public string InstagramPK { get; set; }
+        // Здесь мы храним инстаграм кукис
+        public byte[] StateData { get; set; }
+        // Список подписчиков пользователя
+        public List<ApplicationUser> Followers { get; set; }
+        // Количество подписчиков пользователя (думаю, что это поле можно и убрать)
+        public int FollowersCount { get; set; }
+
+        // Конструктор по умолчанию
+        public ApplicationUser()
+        {
+            Followers = new List<ApplicationUser>();
+            SetFollowersCount();
+        }
+        // Установка значения поля FollowersCount 
+        public void SetFollowersCount()
+        {
+            FollowersCount = Followers.Count;
+        }
+    }
+}
