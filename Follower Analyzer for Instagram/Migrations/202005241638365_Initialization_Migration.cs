7dbc4cff4e6a065718dde65125c590e9a4cb759a
Nejk_4neo@student.itstep.org <Nejk_4neo@student.itstep.org>
2020-05-26 18:09:38 +0300

    аа

---
+++ Follower Analyzer for Instagram/Migrations/202005241638365_Initialization_Migration.cs
@@ -0,0 +1,16 @@
+namespace Follower_Analyzer_for_Instagram.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initialization_Migration : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
