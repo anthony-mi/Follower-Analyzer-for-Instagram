f0cea95fbe850cb546d80fe814ea01137bdda638
Nejk_4neo@student.itstep.org <Nejk_4neo@student.itstep.org>
2020-05-27 23:03:02 +0300

    добавленно свойство (UserActivitys) в класс UserIsMonitored.

--- Follower Analyzer for Instagram/Models/UserIsMonitored.cs
+++ Follower Analyzer for Instagram/Models/UserIsMonitored.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,8 @@ namespace Follower_Analyzer_for_Instagram.Models
     public class UserIsMonitored
     {
         public int Id { get; set; }
+        public string UserPK { get; set; }
         public string UserName { get; set; }
-        public virtual ICollection<InstagramPost> Posts { get; set; }
+        public virtual ICollection<UserActivity> UserActivitys { get; set; }
     }
 }
