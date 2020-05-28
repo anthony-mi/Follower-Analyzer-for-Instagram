24bddc9b3462455d6dc891a1da042b693aab0dc4
Anton <b01a36@i.ua>
2020-05-28 09:14:02 +0300

    Added and implemented `AddTargetUserToObservable` method to `IActivityAnalizer`

--- Follower Analyzer for Instagram/Services/ActivityAnalizer/IActivityAnalizer.cs
+++ Follower Analyzer for Instagram/Services/ActivityAnalizer/IActivityAnalizer.cs
@@ -12,6 +12,7 @@ namespace Follower_Analyzer_for_Instagram.Services.ActivityAnalizer
     {
         void StartAnalizing(CancellationTokenSource cancellationTokenSource);
         void AddUserForObservation(ApplicationUser observer, ObservableUser observable);
+        void AddTargetUserToObservable(ApplicationUser observer, ObservableUser observable, ObservableUser target);
         void RemoveUserFromObservation(ApplicationUser observer, ObservableUser observable);
     }
 }
