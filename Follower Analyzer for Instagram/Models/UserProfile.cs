b710d87d4f1f9804bd2018bbc8828cb20633af4f
Michael <m.kinder853@gmail.com>
2020-06-02 13:23:02 +0300

    Добавлен конструктор в клас InstagramPost

---
+++ Follower Analyzer for Instagram/Models/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Follower_Analyzer_for_Instagram.Models
+{
+    public class UserProfile
+    {
+        [Key]
+        public int Id { get; set; }
+        public User Owner { get; set; }
+        public List<InstagramPost> Posts { get; set; }
+    }
+}
--- Follower Analyzer for Instagram/Models/ViewModels/InstagramPost.cs
+++ Follower Analyzer for Instagram/Models/ViewModels/InstagramPost.cs
@@ -10,5 +10,10 @@ namespace Follower_Analyzer_for_Instagram.Models
         public int CountOfComments { get; set; }
         public List<User> Commenters { get; set; }
         public string MediaFileUri { get; set; }
+        public InstagramPost()
+        {
+            Likers = new List<User>();
+            Commenters = new List<User>();
+        }
     }
 }
